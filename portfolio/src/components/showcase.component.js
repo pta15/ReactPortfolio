5666d88a6d004b1b5ede6807f61b29eaf7c31bbf
Abdihafid <abdihafid123@hotmail.co.uk>
2020-07-27 19:08:07 +0100

    added button and box to showcase

--- portfolio/src/components/showcase.component.js
+++ portfolio/src/components/showcase.component.js
@@ -7,12 +7,15 @@ import "../stylesheets/showcase.component.css"
 export default class Showcase extends Component{
     render(){
       return (
-        <div class="video-size video-grid">
+        <div className="video-size video-grid">
             <video autoPlay muted loop>
             <source src={vid1} type="video/mp4" />
             </video>
-            <div class="text-container">
+            <div className="box"></div>
+            <div className="text-container">
             <h1>Welcome to my portfolio</h1>
+            <h2>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</h2>
+            <button className="button-1"><span>View</span></button>
             </div>
         </div>
     )
